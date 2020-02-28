4438fd205d609d4d4e0fb0328f2e978304c427c7
Javier Hevia <javierhc22@gmail.com>
2020-02-27 19:50:07 -0600

    Prueba1

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp')
 var zip = require('gulp-zip')
 const concat = require('gulp-concat')
+var git = require('gulp-git')
 // const uglify = require('gulp-uglify')
 // se ralizaára la siguiente tarea
 
@@ -20,4 +21,9 @@ gulp.task('compilar', function js () {
   return gulp.src('../*.js') // agarra todos los archivos .js
     .pipe(concat('Compilado.js')) // hace un archivo Compilado.js y copia todo lo que tenía del paso anterior
     .pipe(gulp.dest('Destino')) // lo deposita dentro de una carpeta Destino
+})
+
+gulp.task('commit', function () {
+  return gulp.src('../*')
+    .pipe(git.commit('Prueba1'))
 })
