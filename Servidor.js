afed1f0aabe7ef4c04a4490641b673c466064577
Javier Hevia <javierhc22@gmail.com>
2020-03-05 16:15:55 -0600

    modificacion de servidor.js

--- Servidor.js
+++ Servidor.js
@@ -7,10 +7,18 @@ app.listen(3001, function () {
   console.log('Servidor en http://localhost:3001')
 })
 
-app.get('/', function (req, res) {
-  res.send('Hola mundo!! Express!!')
-})
+// app.get('/', function (req, res) {
+  // res.send('Hola mundo!! Express!!')
+// })
 
 app.get('/bienvenido/:nombre', function (req, res) { // http://localhost:3001/bienvenido/hola
   res.send('Bienvenido!! ' + req.params.nombre + '!!')
 })
+
+
+// var express = require('express');
+// var app = express();
+// var router = express.Router();
+// router.use('/contenido',express.static('/mi_carpeta_en_servidor'));
+// app.use('/',router);
+// app.listen(8080);
